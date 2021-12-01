d6965e0bb0457fcc9575e6d36f99957ff024e590
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-12-01 13:53:25 +0100

    Add files via upload

--- src/main/java/starter/Temperatura.java
+++ src/main/java/starter/Temperatura.java
@@ -13,5 +13,17 @@ public class Temperatura {
 		String mensaje = "La temperatura en fahrenheit es: "+valorF;
 		return mensaje;
 	}
+	
+	public String convertirTemperaturaCaF(float temp) {
+		float valorF = (temp*9/5)+32;
+		String mensaje = "La temperatura en fahrenheit es: "+valorF;
+		return mensaje;
+	}
+	
+	public String convertirTemperaturaFaC(float temp) {
+		float valorC = (temp-32)*(5/9);
+		String mensaje = "La temperatura en celsius es: "+valorC;
+		return mensaje;
+	}
 
 }
